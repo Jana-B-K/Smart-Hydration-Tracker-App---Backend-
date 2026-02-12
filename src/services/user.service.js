ace9941d99b9008b781de333cd09fcd639496efd
Jana <janakumar9843@gmail.com>
2026-02-12 18:14:00 +0530

    add fcm token

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -8,8 +8,24 @@ export const getUserById = async (id) => {
 
 export const updateUser = async (id, data) => {
     const updateduser = await Users.findByIdAndUpdate(id, data, {
-        new: true,
+        returnDocument: 'after',
         runValidators: true,
     });
     return updateduser;
+}
+
+export const setFcmToken = async (id, fcmToken) => {
+    const updatedUser = await Users.findByIdAndUpdate(
+        id,
+        { fcmToken },
+        { returnDocument: 'after', runValidators: true }
+    );
+
+    if (!updatedUser) {
+        const err = new Error('User not found');
+        err.statusCode = 404;
+        throw err;
+    }
+
+    return updatedUser;
 }

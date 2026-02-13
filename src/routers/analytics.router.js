6dec26061244f7880da90bbe4d84c9bee4553e5f
Jana <janakumar9843@gmail.com>
2026-02-13 18:21:31 +0530

    add extra end point

--- src/routers/analytics.router.js
+++ src/routers/analytics.router.js
@@ -4,8 +4,13 @@ import * as analyticsController from '../controllers/analytics.controller.js';
 const analyticsRouter = Router();
 
 analyticsRouter.get('/weekly', analyticsController.getWeeklyData);
+analyticsRouter.get('/daily-history', analyticsController.getDailyHistory);
+analyticsRouter.get('/weekly-performance', analyticsController.getWeeklyPerformance);
 analyticsRouter.get('/monthly', analyticsController.getMonthlyAnalytics);
+analyticsRouter.get('/monthly-comparison', analyticsController.getMonthlyComparison);
 analyticsRouter.get('/streak', analyticsController.streakCalculation);
+analyticsRouter.get('/streak-badge', analyticsController.getStreakBadge);
 analyticsRouter.get('/hydration', analyticsController.calculateHydrationScore);
+analyticsRouter.get('/export', analyticsController.exportReport);
 
 export default analyticsRouter;
